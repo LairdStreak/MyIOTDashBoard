4d0e928e64ad6b0548d0e28013be8d8e246c43cb
lairdstreak <lairdstreak@gmail.com>
2018-06-11 18:25:59 +1200

    add api endpoint

--- MyIOTDashBoard/MyIOTDashBoard/views.py
+++ MyIOTDashBoard/MyIOTDashBoard/views.py
@@ -35,3 +35,33 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route("/api/user/<int:user_id>", methods=["GET"])
+def user_show(user_id):
+    """
+    This function shows a particular user.
+    """
+    #return a particular user
+    print("Retrieve user #{}".format(user_id))
+    result = user_get(user_id)
+    return Response(json.dumps(result), mimetype="application/json")
+
+
+def user_get(user_id):
+    """
+    This function retrieves a user's information.
+    :param user_id: user ID
+    :type user_id: int
+    """
+    #prepare result
+    json = {}
+    results = []
+    temp = {}
+    #get _all_ the information
+    temp[row[0]] = {}
+    temp[row[0]]["id"] = "1"
+    temp[row[0]]["name"] = "test"
+    temp[row[0]]["mail"] = "test2"
+    results.append(temp[row[0]])
+    json["results"] = results
+    return json
